88d3de6581455a21c4ba183c83069a011b452ce0
Luciano <lucianotimo@gmail.com>
2022-08-17 21:13:54 -0300

    advadv

--- src/middlewares/validateRegisterMiddleware.js
+++ src/middlewares/validateRegisterMiddleware.js
@@ -2,17 +2,37 @@ const path = require('path');
 const { body } = require('express-validator');
 
 const validations = [
-  body('name_and_surname').notEmpty().withMessage('Tienes que escribir un nombre y apellido'),
+  body('name_and_surname')
+    .notEmpty()
+    .withMessage('Tienes que escribir un nombre y apellido')
+    .bail()
+    .isLength({ min: 2 })
+    .withMessage('Minimo 2 caracteres'),
   body('email')
-    .notEmpty().withMessage('Tienes que escribir un E-mail').bail()
-    .isEmail().withMessage('Debes escribir un formato de correo válido'),
-  body('address').notEmpty().withMessage('Tienes que escribir una dirección'),
-  body('phone').notEmpty().withMessage('Tienes que escribir un teléfono'),
-  body('country').notEmpty().withMessage('Tienes que elegir un país'),
-  body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+    .notEmpty()
+    .withMessage('Tienes que escribir un E-mail')
+    .bail()
+    .isEmail()
+    .withMessage('Debes escribir un formato de correo válido'),
+  body('address')
+    .notEmpty()
+    .withMessage('Tienes que escribir una dirección'),
+  body('phone')
+    .notEmpty()
+    .withMessage('Tienes que escribir un teléfono'),
+  body('country')
+    .notEmpty()
+    .withMessage('Tienes que elegir un país'),
+  body('password')
+    .notEmpty()
+    .withMessage('Tienes que escribir una contraseña')
+    .bail()
+    .isLength({ min: 8 })
+    .withMessage('Minimo 8 caracteres'),
+
   body('avatar').custom((value, { req }) => {
     let file = req.file;
-    let acceptedExtensions = ['.jpg', '.png', '.gif'];
+    let acceptedExtensions = ['.jpg', '.png', '.gif', '.jpeg'];
 
 
     if (!file) {
